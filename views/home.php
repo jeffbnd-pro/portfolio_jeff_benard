5a390d725b647f166fabbefb963d9b3394ffaf17
Jeff Benard <pro.jeffbenard@gmail.com>
2025-12-18 22:08:26 +0100

    Home page html

--- views/home.php
+++ views/home.php
@@ -1,6 +1,37 @@
 <section id="home-section">
     <div class="title-first">
         <h1><?= htmlspecialchars($title) ?></h1>
-        <p><?= htmlspecialchars($message) ?></p>
+        <p class="subtitle"><?= htmlspecialchars($message) ?></p>
+    </div>
+
+    <div id="card">
+        <div id="img-avatar">
+            <img src="/public/assets/img/avatar_jeff_benard.png" alt="Avatar de Jeff Benard">
+        </div>
+        <div class="card-content">
+            <h2>Développeur Web Junior</h2>
+            <p>
+                Passionné par l'informatique depuis mes premiers plugins de jeux vidéo, j'ai choisi de transformer cette vocation de longue date en carrière professionnelle via une reconversion chez <strong>Need for School</strong>. 
+                Fort d'un parcours riche, marqué par une année en <strong>Australie</strong> et une solide expérience en alternance industrielle, j'apporte aujourd'hui une grande adaptabilité et un regard créatif à mes projets Web. 
+                Actuellement focalisé sur l'architecture <strong>PHP (POO & MVC)</strong>, et sur mon temps libre : <strong>React</strong>/<strong>TypeScript</strong>, je développe des solutions robustes comme ma web-app Garant'IA. 
+                Je recherche une entreprise dès maintenant pour mettre mon autonomie et ma détermination au service de projets ambitieux.
+            </p>
+            <div class="status-badge">
+                <span class="dot"></span> Disponible pour une alternance ! Now
+            </div>
+        </div>
+    </div>
+
+    <div class="tech-stack">
+        <h3>// Stack Technique</h3>
+        <div class="tags">
+            <span class="tag">PHP 8</span>
+            <span class="tag">MySQL</span>
+            <span class="tag">JavaScript</span>
+            <span class="tag">Architecture MVC(Mini-framework homemade)</span>
+            <span class="tag">HTML/CSS</span>
+            <span class="tag">GitHub/GitLab</span>
+            <span class="tag">Git</span>
+        </div>
     </div>
 </section>
